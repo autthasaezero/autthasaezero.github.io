3f9eaf0bdbda81d3a51f749a510a6b2caf422332
autthasaezero <autthasaezero@gmail.com>
2021-08-25 20:03:40 +0700

    194a604

---
+++ _nuxt/5113f2d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{292:function(e,n,t){"use strict";t.r(n);var l={},o=t(43),component=Object(o.a)(l,(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("Home")],1)}),[],!1,null,null,null);n.default=component.exports;installComponents(component,{Home:t(291).default})}}]);
